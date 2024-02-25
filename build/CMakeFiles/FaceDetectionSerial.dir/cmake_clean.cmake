93bed3253de8ac0e606d158685dd95a9a571d62f
infopcgood <112328126+infopcgood@users.noreply.github.com>
2024-02-25 13:04:27 +0900

    Add build folder to git

---
+++ build/CMakeFiles/FaceDetectionSerial.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/FaceDetectionSerial.dir/main.cpp.o"
+  "CMakeFiles/FaceDetectionSerial.dir/main.cpp.o.d"
+  "FaceDetectionSerial"
+  "FaceDetectionSerial.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/FaceDetectionSerial.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
